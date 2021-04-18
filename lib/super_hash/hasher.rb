9ba60bdd0f2cd62a25eb60e5300938b7b836489f
Pato <pato_devilla@hotmail.com>
2021-04-18 17:53:08 -0500

    improve initialize performance

--- lib/super_hash/hasher.rb
+++ lib/super_hash/hasher.rb
@@ -10,7 +10,7 @@ end
 module SuperHash
   class Hasher < ::Hash
     
-    #ToDo only include if configured
+    # ToDo only include if configured
     include Types
   
     # The idea of the SuperHash is to have hashes with extended
@@ -52,42 +52,59 @@ module SuperHash
     #   attribute? :key_name, transform: All_OF_PROC
     #
   
-    #registers an after_set callback
+    # registers an after_set callback
+    # @param block [Proc] Proc to call after each set
+    # @return [Proc] added proc
     def self.after_set(block)
+      raise TypeError.new("Expected Proc, got #{block.class}") unless block.is_a? Proc
       after_set_callbacks.push(block)
     end
   
-    #registers an none required attribute
+    # registers an NONE REQUIRED attribute
+    # @param attribute_name [Symbol] name of attribute
+    # @param options [Hash] Symbolized hash
+    # @return [Class] class where attribute was added
     def self.attribute?(attribute_name, options = {})
       options = options.merge({required: false})
-      _register_attribute(attribute_name, options)
+      register_attribute(attribute_name, options)
     end
   
-    #registers a required attribute
+    # registers an REQUIRED attribute
+    # @param attribute_name [Symbol] name of attribute
+    # @param options [Hash] Symbolized hash
+    # @return [Class] class where attribute was added
     def self.attribute(attribute_name, options = {})
       options = options.merge({required: true})
-      _register_attribute(attribute_name, options)
+      register_attribute(attribute_name, options)
     end
 
     #updates or registers an attribute
+    # @param attribute_name [Symbol] name of attribute
+    # @param options [Hash] Symbolized hash
+    # @return [Class] class where attribute was added
     def self.update_attribute(attribute_name, options = {})
       options = (attributes[attribute_name] || {}).merge(options)
-      _register_attribute(attribute_name, options)
+      register_attribute(attribute_name, options)
     end
 
-    #unregisters an attribute
+    # unregisters an attribute
+    # @param attribute_name [Symbol] name of attribute
+    # @return [Hash] Hash of remaining attributes
     def self.remove_attribute(attribute_name)
       instance_variable_set('@attributes', attributes.reject{|k,v| k == attribute_name})
     end
   
-    #The actual attribute registration method.
-    def self._register_attribute(attribute_name, options)
+    # The actual attribute registration method.
+    # @param attribute_name [Symbol] name of attribute
+    # @param options [Hash] Symbolized hash
+    # @return [Class] class where attribute was added
+    def self.register_attribute(attribute_name, options)
       raise TypeError.new('attribute_name must be a symbol') unless attribute_name.is_a?(::Symbol)
       attributes[attribute_name] = options
       self
     end
   
-    #class level getter for attributes
+    # class level getter for attributes
     class << self
       attr_reader :attributes
       attr_reader :after_set_callbacks
@@ -118,13 +135,17 @@ module SuperHash
     end
   
     # Check to see if the specified attribute has been defined.
+    # @param name [Symbol] name of attribute
+    # @return [Boolean]
     def self.has_attribute?(name)
-      !attributes.find{|prop, options| prop == name}.nil?
+      !attributes[name].nil?
     end
   
     # Check to see if the specified attribute is required.
+    # @param name [Symbol] name of attribute
+    # @return [Boolean]
     def self.attr_required?(name)
-      !attributes.find{|prop, options| prop == name && options[:required]}.nil?
+      attributes.dig(name, :required) == true
     end
   
     #allow a specific instance not to require a set of attributes
@@ -134,21 +155,26 @@ module SuperHash
     def initialize(init_value = nil, options={})
       instance_variable_set('@options', options || {})
 
+      # allow some custom initialization with a proc
       if options[:preinit_proc]
         options[:preinit_proc].call(self)
       end
       
       if init_value.is_a? ::Hash
+        allow_dynamic = self.class.allow_dynamic_attributes
+        set_callbacks = self.class.after_set_callbacks
+        # ToDo remove this for optimal performance
         init_value = SuperHash::DeepKeysTransform.symbolize_recursive(init_value)
   
         #set init_value
         init_value.each do |att, value|
-          self[att] = value
+          self.[]=(att, value, true, allow_dynamic, set_callbacks)
         end
       end
 
-      #set defaults
-      self.class.attributes.each do |name, options|
+      # set defaults
+      ignore_nil_default_values = self.class.ignore_nil_default_values
+      klass_attributes.each do |name, options|
         next if self.key?(name)
         if options.key?(:default) && options[:type]&.default?
           raise ArgumentError.new('having both default and type default is not supported')
@@ -169,36 +195,49 @@ module SuperHash
         elsif options[:type]&.default?
           options[:type][Dry::Types::Undefined]
         end
-        self[name] = default_value unless default_value.nil? && !attr_required?(name) && self.class.ignore_nil_default_values
+        self[name] = default_value unless default_value.nil? && !attr_required?(name) && ignore_nil_default_values
       end
 
       super(init_value) unless init_value.is_a? ::Hash
   
       validate_all_attributes!
     end
+
+    # Gets attribute definition from owner class
+    # @param name [Symbol] name of attribute
+    # @return [Hash|Nil] definition of the attribute
+    def klass_attribute(name)
+      self.class.attributes[name]
+    end
+
+    # Gets all definitions from owner class
+    # @return [Hash] definition of all attribute
+    def klass_attributes
+      self.class.attributes
+    end
     
+    # runs validate_attribute for all attributes
+    # @return [Hash] definition of all attribute
     def validate_all_attributes!
-      self.class.attributes.each do |name, options|
+      klass_attributes.each do |name, options|
         validate_attribute!(name)
       end
     end
   
-    #run the following validations to an attribute:
-    # TODO maybe other validations may apply?
-    # 1) required
+    # run the following validations to an attribute:
+    #   - if required and present
+    # @return [Nil]
     def validate_attribute!(name, value=nil)
-      prop = self.class.attributes.find{|prop, options| prop == name }
-      name = prop[0]
+      # prop = klass_attribute(name)
       value = value || self[name]
-      # options = prop[1]
       if value.nil? && attr_required?(name)
         raise SuperHash::Exceptions::PropertyError, "The attribute '#{name}' is required"
       end
     end
   
     # Retrieve a value
+    # ToDo: verify this logic
     def [](attribute)
-      # assert_attribute_exists! attribute
       value = super(attribute)
       # If the value is a lambda, proc, or whatever answers to call, eval the thing!
       if value.is_a? Proc
@@ -211,38 +250,33 @@ module SuperHash
   
     # Set a value. Only works on pre-existing attributes,
     # unless @allow_dynamic_attributes is true.
-    def []=(attribute, value)
-      if !attribute.is_a? ::Symbol
+    def []=(attribute, value, skip_validate_attribute = nil, allow_dynamic_attributes = nil, after_set_callbacks=nil)
+      if !attribute.is_a? ::Symbol #15% performance loss
         raise TypeError.new('only symbols are supported as attributes')
       end
-  
-      should_run_logic = if self.class.allow_dynamic_attributes
-        self.class.has_attribute?(attribute)
-      else
-        true
+      
+      validate_attribute!(attribute, value) unless skip_validate_attribute
+
+      unless allow_dynamic_attributes || self.class.allow_dynamic_attributes
+        assert_attribute_exists!(attribute)
       end
-  
-      if should_run_logic
-        assert_attribute_exists! attribute
-        validate_attribute!(attribute, value)
-  
+
+      attribute_def = klass_attribute(attribute)
+      if attribute_def
         #transform value with transform
-        transform = self.class.attributes[attribute][:transform]
-        if !transform.nil?
-          if transform.is_a?(Proc)
-            value = transform.call(self, value, attribute)
-          end
-        end
+        transform = attribute_def[:transform]
+        value = transform.call(self, value, attribute) if transform.is_a? Proc
+
         #transform value with type
-        type = self.class.attributes[attribute][:type]
-        if !type.nil?
-          value = type[value]
-        end
+        type = attribute_def[:type]
+        value = type[value] unless type.nil?
       end
       
       super(attribute, value)
-  
-      self.class.after_set_callbacks.each{|proc| instance_exec(attribute, value, &proc)}
+      
+      (after_set_callbacks || self.class.after_set_callbacks).each do |proc|
+        instance_exec(attribute, value, &proc)
+      end
     end
   
     private
